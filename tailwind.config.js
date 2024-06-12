668df691ff450304ed09b6dd95364ef48878c840
MrLkuch <kucheida.lucas@gmail.com>
2024-06-12 18:04:55 +0200

    style(tailwind.config.js): add new classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      rotate: {
+        '270': '270deg',
+      },
+      inset: {
+        '60%': '60%',
+      }
+    },
   },
   plugins: [],
 }
