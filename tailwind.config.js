cf1820220abf02a7a09c8187a2351a7360faf93f
MrLkuch <kucheida.lucas@gmail.com>
2024-06-18 11:30:37 +0200

    style(tailwind.config.js): add font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        Anek: ["Anek Devanagari", "sans-serif"]
+      },
       rotate: {
         '270': '270deg',
       },
@@ -11,7 +14,8 @@ module.exports = {
       },
       boxShadow: {
         'card': '5px 5px 10px 0px rgba(13, 148, 136, 0.5), -5px -5px 10px -5px rgba(13, 148, 136,1)',
-      }
+      },
+
     },
   },
   plugins: [],
